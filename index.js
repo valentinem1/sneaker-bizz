99cc059d4ef1751fd1c8c62ed2faf020e3a2b992
Valentine <valentinemaillard1@gmail.com>
2020-08-18 10:02:59 -0400

    added sneaker list to home page

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 $(document).ready(function(){
 
-    // creates div to hold header
-    const pageHeaderContainer = document.createElement('div');
-    pageHeaderContainer.className = 'page-header-container';
+    // fetches sneakers from external API
+    function fetchSneakers() {     
+        fetch('https://api.thesneakerdatabase.com/v1/sneakers?limit=100')
+        .then(r => r.json())
+        .then(data => {
+            // call the function to display each sneaker onto the DOM.
+            displaySneakersOnDOM(data.results)
+        })
+        .catch(err => console.log(err))
+    }
+    fetchSneakers();
+    
+    function displaySneakersOnDOM(sneakerData) {
 
-    // creates header
-    const homePageHeader = document.createElement('h1');
-    homePageHeader.className = 'page-header-title';
-    homePageHeader.innerText = "Sneaker Bizz";
+        // creates a div wrapper for all the sneakers.
+        const divWrapper = document.createElement('div');
+        divWrapper.className = 'div-wrapper';
+        $('body').append(divWrapper);
 
-    // append it to the div and then append the div to the body of html
-    pageHeaderContainer.appendChild(homePageHeader);
-    $("body").append(pageHeaderContainer);
+        // iterates over each sneaker to create dom elements for it to display it.
+        sneakerData.forEach(sneaker => {
+            if(sneaker.year < 2021){
+                // creates sneaker card div.
+                const sneakerCard = document.createElement('div');
+                sneakerCard.className = 'sneaker-card';
+                
+                // creates sneaker header card div.
+                const sneakerCardHeader = document.createElement('div');
+                sneakerCardHeader.className = 'sneaker-card-header';
+        
+                // creates sneaker header.
+                const sneakerHeader = document.createElement('h2');
+                sneakerHeader.className = 'sneaker-header';
+                sneakerHeader.innerText = `${sneaker.brand}, ${sneaker.name}`
+    
+                // append h2 to sneakerCardHeader div.
+                sneakerCardHeader.appendChild(sneakerHeader);
+                // append sneakerCard Header div to the SneakerCard div.
+                sneakerCard.appendChild(sneakerCardHeader);
+                // append SneakerCard div to the div wrapper.
+                divWrapper.appendChild(sneakerCard);
+            }
+        });
 
+    }
 });
